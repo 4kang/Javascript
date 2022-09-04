7017ea92b8a06bd2df06ff839ea28db8c9a3dd57
4kang <106650699+4kang@users.noreply.github.com>
2022-09-04 06:55:28 +0000

    Add appjs

--- app.js
+++ app.js
@@ -17,13 +17,30 @@
 
 // alert('Hello' + 'World');
 
-let orange = 100;
-let apple = 120;
 
-if(orange < apple){
-  alert('みかんの値段がりんごより安い')
-} else if(orange == apple){
-  alert('みかんとりんごが同じ値段')
-} else{
-  alert('みかんの値段がりんごより高い')
-}
+// if文
+// let orange = 100;
+// let apple = 120;
+
+// if(orange < apple){
+//   alert('みかんの値段がりんごより安い')
+// } else if(orange == apple){
+//   alert('みかんとりんごが同じ値段')
+// } else{
+//   alert('みかんの値段がりんごより高い')
+// }
+
+
+// while文
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1 ;
+// }
+
+// alert('2をかけ続けて' + max + 'を超えるのに必要だった回数は' + count + "回です");
+
+
