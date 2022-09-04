16c074c717a7cc87e263916ef9a831735e6c0b32
4kang <106650699+4kang@users.noreply.github.com>
2022-09-04 06:49:14 +0000

    Add appjs

---
+++ app.js
@@ -0,0 +1,29 @@
+// let 変数名 = 内容;
+// let hello = "Hello World"
+// alert(hello);
+
+
+// 足し算
+// alert(4 + 3);
+
+// 引き算
+// alert(8 - 5);
+
+// 掛け算
+// alert(2 * 6);
+
+// 割り算
+// alert(10 / 2);
+
+// alert('Hello' + 'World');
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い')
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段')
+} else{
+  alert('みかんの値段がりんごより高い')
+}
