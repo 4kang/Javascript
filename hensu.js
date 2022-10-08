28ca95c799ea68c6c886e4c11419909b4f4a5ea4
4kang <106650699+4kang@users.noreply.github.com>
2022-10-08 13:11:20 +0000

    Add hensu10/8

--- hensu.js
+++ hensu.js
@@ -5,3 +5,21 @@
 // console.log(nickname)
 // var nickname = "ichiro"
 // console.log(nickname)
+
+// -----------letによる再宣言-------------※エラー出る
+// let nickname = "taro"
+// console.log(nickname)
+// let nickname = "ichiro"
+// console.log(nickname)
+
+// -----------constによる再宣言-------------※エラー出る
+// let nickname = "taro"
+// console.log(nickname)
+// let nickname = "ichiro"
+// console.log(nickname)
+
+// -----------varによる再代入----------------
+var nickname = "taro"
+console.log(nickname)
+var nickname ="jiro"
+console.log(nickname)
