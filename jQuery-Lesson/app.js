6ab73c97cbbb097ec1e118f87167394ca3032f92
4kang <106650699+4kang@users.noreply.github.com>
2022-10-22 14:34:34 +0000

    Add index

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,4 +1,10 @@
 /* global $*/
-$(function(){
-  $('.box1').hide();
+$(function (){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
 });
