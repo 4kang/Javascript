b49b8d4252a0bdd745d8f9e167c5f97a8ff5d7a7
4kang <106650699+4kang@users.noreply.github.com>
2022-10-24 06:23:23 +0000

    Add 10/24

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,4 +1,5 @@
 /* global $*/
+
 // $(function (){
 //   $('.box1').slideDown(function (){
 //     $('.box1').css({
@@ -9,11 +10,35 @@
 //   });
 // });
 
+
+// マウスオーバーで色を変える
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000FF'});
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color': '#FF0000'});
+//   });
+// });
+
+
+// マウスオーバーで線を囲む
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+
+// .box1がクリックされたときは、box1-extのclassを追加する。マウスアウトされたときは、box1-extのclassを削除する
 $(function(){
-  $('.box1').mouseover(function(){
-    $('.box1').css({'background-color': '#0000FF'});
+  $('.box1').on('click',function(){
+    $('.box1').addClass('box1-ext');
   });
   $('.box1').mouseout(function(){
-    $('.box1').css({'background-color': '#FF0000'});
+    $('.box1').removeClass('box1-ext');
   });
 });
